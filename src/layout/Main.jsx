d6c7a5f9a9b9cba11d20f74aef4022710ad581e4
Igor Kudlo <igor.kudlo@spiralscout.com>
2022-04-15 17:16:38 +0300

    add Preloader, Search

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react"
+import Preloader from "../components/Preloader"
 import Movies from "../components/Movies"
+import Search from "../components/Search"
 
 class Main extends React.Component {
     state = {
@@ -12,17 +14,23 @@ class Main extends React.Component {
             .then(data => this.setState({ movies: data.Search }))
     }
 
+    searchMovies = (str) => {
+        fetch(`http://www.omdbapi.com/?apikey=11e5b044&s=${str}`)
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+
     render() {
         const {movies} = this.state;
 
         return (
             <main className="container content">
+                <Search searchMovies={this.searchMovies}/>
                 {
                     movies.length
                         ? <Movies movies={movies} />
-                        : <h3>Loading ...</h3>
+                        : <Preloader/>
                 }
-
             </main>
         )
     }
