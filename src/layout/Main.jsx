c0aa83a67064b386f4991a56c5a885401b4be842
Igor Kudlo <igor.kudlo@spiralscout.com>
2022-04-15 14:23:28 +0300

    get data from server, display data

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,31 @@
-function Main() {
-    return (
-        <main className="container content">
-            Movies App
-        </main>
-    )
+import React from "react"
+import Movies from "../components/Movies"
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=11e5b044&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+
+    render() {
+        const {movies} = this.state;
+
+        return (
+            <main className="container content">
+                {
+                    movies.length
+                        ? <Movies movies={movies} />
+                        : <h3>Loading ...</h3>
+                }
+
+            </main>
+        )
+    }
 }
 
 export default Main
